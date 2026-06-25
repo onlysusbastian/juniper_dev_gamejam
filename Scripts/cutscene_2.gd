3b1c8778e04c784012c6cb00b8a033d069f64d2c
onlysusbastian <clockworkninja69@gmail.com>
2026-06-25 16:50:25 +0530

    cutscene fix
    sdf

--- Scripts/cutscene_2.gd
+++ Scripts/cutscene_2.gd
@@ -1,15 +1,105 @@
-extends Node2D
+extends CanvasLayer
+
+@export var next_scene : String
+
+var dialogues = []
+var typing_sound_timer := 0.0
+var current_index := 0
+var current_label : RichTextLabel
+
+var typing := false
+var char_timer := 0.0
+var char_delay := 0.03
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+
+	randomize()
+
+	for child in get_children():
+
+		if child.name.begins_with("Dialogue"):
+
+			dialogues.append(child)
+			child.hide()
+
+	show_dialogue(0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+
+	if !typing:
+		return
+
+	char_timer += delta
+
+	if char_timer >= char_delay:
+
+		char_timer = 0.0
+
+		current_label.visible_characters += 1
+
+		# Play typing sound for every character
+		$TypingSound.pitch_scale = randf_range(0.3,1.3)
+		#if !$TypingSound.playing:
+		$TypingSound.play()
+		typing_sound_timer = 0.03
+
+		if current_label.visible_characters >= current_label.get_total_character_count():
+
+			typing = false
 
 
-func _on_continue_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_2.tscn")
+func _input(event):
+
+	if !event.is_action_pressed("ui_accept"):
+		return
+
+	if typing:
+
+		current_label.visible_characters = current_label.get_total_character_count()
+
+		typing = false
+
+		$TypingSound.stop()
+
+	else:
+
+		# Stop current dialogue voice
+		for child in dialogues[current_index].get_children():
+
+			if child is AudioStreamPlayer:
+				child.stop()
+
+		dialogues[current_index].hide()
+
+		current_index += 1
+
+		if current_index >= dialogues.size():
+
+			get_tree().change_scene_to_file(next_scene)
+
+		else:
+
+			show_dialogue(current_index)
+
+
+func show_dialogue(index):
+
+	dialogues[index].show()
+
+	for child in dialogues[index].get_children():
+
+		if child is RichTextLabel:
+
+			current_label = child
+
+		elif child is AudioStreamPlayer:
+
+			child.play()
+
+	current_label.visible_characters = 0
+
+	char_timer = 0.0
+
+	typing = true
